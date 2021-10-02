b44a4601a88a34337367f0bc7b2281581f39c00f
reppon97 <reppon97@gmail.com>
2021-10-02 14:16:24 +0300

    Allowed hosts & docker compose command

--- core/settings.py
+++ core/settings.py
@@ -9,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'ubuntu.test']
+ALLOWED_HOSTS = ["45.93.6.221", "reppon.live"]
 
 
 # Application definition
